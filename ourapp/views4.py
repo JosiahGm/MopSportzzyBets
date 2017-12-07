bba530efe3ba56daf986ce77f72e42cb95a7ec87
JosiahGm <32169491+JosiahGm@users.noreply.github.com>
2017-12-07 15:55:55 +0200

    Rename views.py to views4.py

---
+++ ourapp/views4.py
@@ -0,0 +1,17 @@
+# ourapp/views.py
+
+@app.route('/confirm/<token>')
+def confirm_email(token):
+    try:
+        email = ts.loads(token, salt="email-confirm-key", max_age=86400)
+    except:
+        abort(404)
+
+    user = User.query.filter_by(email=email).first_or_404()
+
+    user.email_confirmed = True
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(url_for('signin'))
