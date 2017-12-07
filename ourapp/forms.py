b1b8d439f15cb8dc3618396cfa8986d87f127b56
JosiahGm <32169491+JosiahGm@users.noreply.github.com>
2017-12-07 15:58:06 +0200

    Update forms.py

--- ourapp/forms.py
+++ ourapp/forms.py
@@ -1,9 +1,24 @@
 # ourapp/forms.py
 
-from flask_wtf import Form
+rom flask_wtf import Form
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired
+
+from .util.validators import Unique
+from .models import User
 
 class EmailPasswordForm(Form):
-    email = StringField('Email', validators=[DataRequired(), Email()])
+    email = StringField('Email', validators=[DataRequired(), Email(),
+        Unique(
+            User,
+            User.email,
+            message='There is already an account with that email.'])
     password = PasswordField('Password', validators=[DataRequired()])
+
+<form action="" method="post">
+    {{ form.email.label }}: {{ form.email(placeholder='yourname@email.com') }}
+    <br>
+    {{ form.password.label }}: {{ form.password }}
+    <br>
+    {{ form.csrf_token }}
+</form>
